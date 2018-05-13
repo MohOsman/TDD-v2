f527600104884ca10279f5d2631ccec98a4423eb
mohosman <mohamed.osman.dev@gmail.com>
2018-05-13 16:46:33 +0200

    added more tests

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,5 +1,8 @@
+import exceptions.NegativNumberException;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
@@ -7,36 +10,62 @@ public class StringCalculatorTest {
 
     private StringCalculator sc ;
 
+    @Rule
+    public ExpectedException thrown =  ExpectedException.none();
+
     @Before
     public void setUp() throws Exception {
         sc = new StringCalculator();
     }
 
     @Test
-    public void empty_string_should_retrun_0() {
+    public void empty_string_should_retrun_0() throws NegativNumberException {
         int result = sc.getSumOfNr("");
 
         assertEquals(0,result);
     }
 
     @Test
-    public void single_Number_returns_the_value() {
+    public void single_Number_returns_the_value() throws NegativNumberException {
           int result = sc.getSumOfNr("4");
           assertEquals(4,result);
 
     }
 
     @Test
-    public void two_numbers_with_comma_should_return_the_sum() {
+    public void two_numbers_with_comma_should_return_the_sum() throws NegativNumberException {
         int result = sc.getSumOfNr("10,20");
         assertEquals(30,result);
         
     }
 
     @Test
-    public void two_numbers_with_newLine_returns_the_sum() {
+    public void two_numbers_with_newLine_returns_the_sum() throws NegativNumberException {
         int result = sc.getSumOfNr("1\n2");
         assertEquals(3,result);
+
+    }
+
+    @Test
+    public void three_numbers_delimited_either_way_return_the_sum() throws NegativNumberException {
+        int result =  sc.getSumOfNr("1\n2,3\n4");
+        assertEquals(10,result);
+    }
+
+
+    @Test
+    public void negativ_numbers_thows_negativNumberException() throws NegativNumberException {
+
+        thrown.expect(NegativNumberException.class);
+        thrown.expectMessage("negatives not allowed:");
+        sc.getSumOfNr("-1");
+    }
+
+
+    @Test
+    public void numbers_More_1000_are_ignord() throws NegativNumberException {
+        int result = sc.getSumOfNr("1100,200");
+        assertEquals(0, result);
 
     }
 }
